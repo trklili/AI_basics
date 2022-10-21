ae0960217049a9a2ea4119f1fd86c6f3dbde56d9
trklili2 <98692529+trklili2@users.noreply.github.com>
2022-10-21 16:22:37 +0200

    lead

--- elso.py
+++ elso.py
@@ -1,3 +1,18 @@
-f = open("ip.txt", 'r')
-print (f.read())
-print(f.readline())
+from itertools import count
+import pandas as pd
+import numpy as np
+df = pd.read_csv('ip.txt', header = None,dtype="string")
+
+ipcimek=df.squeeze()
+
+print("Sorok száma :",ipcimek.shape[0])
+
+
+nodevice=np.sum(ipcimek.str.startswith("2001:0db8", na=False))
+print("Dokumentációs címek száma,amik eszközöknek nincsenek kiosztva:", nodevice)
+
+globalip=np.sum(ipcimek.str.startswith("2001:0e", na=False))
+print("Informatikai eszközöknek kiosztott globális egyedi címek száma", globalip)
+
+localuniqueip=np.sum(ipcimek.str.startswith("fc", na=False))+np.sum(ipcimek.str.startswith("fd", na=False))
+print("Az eszközöknek kiosztott helyi egyedi címek száma", localuniqueip)
